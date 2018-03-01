422d9c97b8b2c11348ad50f7365bc7e047a7d0a7
Tomasz Kornuta <tkornut@us.ibm.com>
2018-02-28 19:26:41 -0800

    algo rotation + reverse

--- algo/reverse_individual_words.py
+++ algo/reverse_individual_words.py
@@ -1,22 +1,82 @@
+# https://www.programcreek.com/2014/05/leetcode-reverse-words-in-a-string-ii-java/
+
+#Given s = "the sky is blue",
+#return "blue is sky the".
+
+import math
+
+def reverse_array(arr,  fi,  li):
+    """ Reverses array from position fi to li
+    O(1) space and O(n) operations -> n/2 * 2 swaps.
+    """
+    for i in range(math.ceil((li-fi)/2)):
+        # Calculate "mirrored" indices
+        ii = fi + i
+        jj = li - i
+        #print("ii={}, jj={}".format(ii, jj))
+        # Swap.
+        tmp = arr[ii]
+        arr[ii] = arr[jj]
+        arr[jj] = tmp
 
 def rev(str):
-    list = str.split()
-    reversed_str = ""
-    # Remove in reversed order.
-    while len(list) > 0:
-            w = list.pop()
-            for l in range(len(w)-1, -1,  -1) :
-                reversed_str = reversed_str + w[l]
-            reversed_str = reversed_str  + " "
-    # Print the result.
-    print (reversed_str)
+    print(str)
+    arr = list(str)
+    print(arr)
+    
+    # Empty list.
+    if (len(arr) <1):
+        print("Edge case 0")
+        return str
 
-def rev2(str):
-    print (str[::-1])
+    # Find first space.
+    i=0
+    if (arr[i] == ' '):
+        while (i < len(arr)) and (arr[i] == ' '):
+            i = i+1
+    # Ok, got the letter.
+ 
+    # Edge case - no words
+    if (i >= len(arr)):
+        print("Edge case 1")
+        return str
+
+    # Go and reverse words, one by one.
+    j=i+1
+    while(j < len(arr)):
+        # Skip letters forming given word.
+        while (j < len(arr)) and (arr[j] != ' ') :
+            j = j+1
+
+        #print("i={}, j={}".format(i, j))
+        # Else: reverse the word
+        reverse_array(arr,  i,  j-1)
+        
+        # And move to the next word.
+        i=j+1
+        # Skip spaces
+        while (i < len(arr)) and (arr[i] == ' '):
+                i = i+1
+       # Ok, got letter (or end of the word)
+        j = i+1
+        
+    # At the end: reverse the whole array.
+    reverse_array(arr,  0, len(arr)-1)
+    
+    # We could use list slicing - but it creates new list, we O(1) space won't hold anymore.
+    # arr = arr[::-1]
+    
+    return ''.join(arr)
+
     
 if __name__ == "__main__":
-    rev("Hello World")
-    rev("Geeks for Geeks")
+    str = "Hello World"
+    str = "the sky is blue" 
+    #str = "abcde"
+    #str="a    alka    abd "
+    print("")
+    print(rev(str))
+    
     
 
 
---
+++ algo/rotate_array.py
@@ -0,0 +1,83 @@
+# https://www.programcreek.com/2015/03/rotate-array-in-java/
+
+import math
+
+def rotate_list(arr,k):
+    """ First idea: jump between adrresses by k as many times as num of elements and overwrite values with ones from previous address.
+    O(2) space and O(n) time
+    """
+    # Length of array.
+    arl = len(arr)
+
+    # Edge cases I & II: k=0 or empty list
+    if arl < 2 or k == 0:
+        print("Edge case")
+        return arr
+
+    # Previous index and element.
+    prev_ind = 0
+    prev_val = arr[0]
+
+    # Perform the shift n times.
+    for _ in range(arl):
+        # Jump to "destination address adrres 
+        ind = (prev_ind + k) % arl
+        #print(ind)
+        # Store variable.
+        val = arr[ind]
+        # Overrite variable.
+        arr[ind] =prev_val
+        # "Update"
+        prev_ind = ind
+        prev_val = val
+    
+    return arr
+
+def swap_symmetric(arr,  i, first,  arl):
+    """ Swaps two array elements lying on the oposite sides of vector of length arl"""
+    #print("i+first={}".format(i+first))
+    tmp = arr[i+first]
+    #print("first + ((arl-1-i) % arl)={}".format(first + ((arl-1-i) % arl)))
+    j = first + ((arl-1-i) % arl)
+    arr[i+first] = arr[j]
+    arr[j] = tmp
+    
+def rotate_list2(arr,k):
+    """ Found idea: Use three reverses.
+    O(1) space and O(2*n) time
+    """
+    # Length of array.
+    arl = len(arr)
+    
+    # Edge cases I & II: k=0 or empty list
+    if arl < 2 or k == 0:
+        print("Edge case")
+        return arr
+    
+    # Modulo the shift.
+    if (k>arl)or (k<0):
+        k = k % arl
+
+    # Reverse the whole list - swap.
+    for i in range(math.floor(arl/2)):
+        swap_symmetric(arr,  i,  0,  arl)
+        
+    #print("reswap1")
+    # Re-swap first k elements
+    for i in range(math.floor(k/2)):
+        swap_symmetric(arr,  i,  0,  k)
+        
+    #print("reswap2")
+    # Re-swap the rest of elements
+    for i in range(math.floor((arl - k)/2)):
+        swap_symmetric(arr,  i,  k, arl-k)
+
+    # Return the array.
+    return arr
+    
+    
+ 
+if __name__ == "__main__":
+    arr = [1,2,3,4,5,6]
+    #arr = [1, 2, 3]
+    print(rotate_list2(arr, -2))
